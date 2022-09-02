40e8d65abfa213634b6fd4450ef62f685e715ad8
nicksaravia <nick.saravia@willowtreeapps.com>
2022-09-02 14:58:49 -0400

    add reason to deprecated function

--- src/resolvers.js
+++ src/resolvers.js
@@ -9,7 +9,6 @@ const resolvers = {
     track: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getTrack(id);
     },
-
     // get a single module by ID, for the module detail page
     module: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getModule(id);
